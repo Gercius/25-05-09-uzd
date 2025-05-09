b6f541394461ea5e832478c65873d37a29d2691a
Gercius <gerciuswork@gmail.com>
2025-05-09 14:03:18 +0300

    format with prettier

--- frontend/src/components/HotelDetails.jsx
+++ frontend/src/components/HotelDetails.jsx
@@ -3,16 +3,25 @@ const HotelDetails = ({ hotel, onEdit, onDelete }) => {
         <div className="col">
             <div className="card">
                 <div className="card-body">
-                    <h5 className="card-title">{hotel.name || 'Be pavadinimo'}</h5>
+                    <h5 className="card-title">{hotel.name || "Be pavadinimo"}</h5>
                     <p className="card-text">
-                        <strong>Aprašymas:</strong> {hotel.description || 'Nėra aprašymo'}<br />
-                        <strong>Adresas:</strong> {hotel.address || 'Nėra adreso'}<br />
-                        <strong>Reitingas:</strong> {hotel.ranking || 'Nėra reitingo'}<br />
+                        <strong>Aprašymas:</strong> {hotel.description || "Nėra aprašymo"}
+                        <br />
+                        <strong>Adresas:</strong> {hotel.address || "Nėra adreso"}
+                        <br />
+                        <strong>Reitingas:</strong> {hotel.ranking || "Nėra reitingo"}
+                        <br />
                     </p>
                     <div className="card-actions">
-                        <button onClick={() => onTasks(tasks)} className="btn btn-sm btn-primary">Peržiūrėti</button>
-                        <button onClick={() => onEdit(contest)} className="btn btn-sm btn-warning">Redaguoti</button>
-                        <button onClick={() => onDelete(contest)} className="btn btn-sm btn-danger">Ištrinti</button>
+                        <button onClick={() => onTasks(tasks)} className="btn btn-sm btn-primary">
+                            Peržiūrėti
+                        </button>
+                        <button onClick={() => onEdit(contest)} className="btn btn-sm btn-warning">
+                            Redaguoti
+                        </button>
+                        <button onClick={() => onDelete(contest)} className="btn btn-sm btn-danger">
+                            Ištrinti
+                        </button>
                     </div>
                 </div>
             </div>
--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import '../styles/base/footer.scss';
+import React from "react";
+import "../styles/base/footer.scss";
 
 function Footer() {
-  return (
-    <footer className="bg-dark text-white text-center">
-        <p>&copy; 2025 Hotel Informastio System - Visos teisės saugomos</p>
-    </footer>
-  );
+    return (
+        <footer className="bg-dark text-white text-center">
+            <p>&copy; 2025 Hotel Informastio System - Visos teisės saugomos</p>
+        </footer>
+    );
 }
 
 export default Footer;
